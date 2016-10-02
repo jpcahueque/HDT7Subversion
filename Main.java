0cdf546b44874b22805627579d8b04d5872e481c
jpcahueque <jpcahueque@gmail.com>
2016-10-02 15:06:20 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JuanPablo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        String mensajeUsuario;
+        System.out.println("Ingrese su mensaje:");
+        mensajeUsuario = teclado.nextLine();
+        
+        char [] arrayMensaje = mensajeUsuario.toCharArray();
+        
+        for(char i:arrayMensaje){
+            arrayMensaje.
+        }
+    }
+    
+}
