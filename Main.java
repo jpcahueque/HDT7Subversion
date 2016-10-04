11d9f32e73a33f41448efa0794048fddf4afdd2a
jpcahueque <jpcahueque@gmail.com>
2016-10-03 19:49:51 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@
  */
 package hdt7;
 
+import java.util.*;
 import java.util.Scanner;
 
 /**
@@ -18,16 +19,57 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Se le pide al usuario que ingrese la cadena de caracteres
         Scanner teclado = new Scanner(System.in);
         String mensajeUsuario;
         System.out.println("Ingrese su mensaje:");
         mensajeUsuario = teclado.nextLine();
         
-        char [] arrayMensaje = mensajeUsuario.toCharArray();
+        ArrayList inicial = new ArrayList<Nodo>();
         
-        for(char i:arrayMensaje){
-            arrayMensaje.
+        boolean valido = true;
+        int contador = 0;
+        //Se convierte el mensaje a una array de chars
+        char [] arrayMensaje = mensajeUsuario.toCharArray();
+        //Se cuenta la frecuencia de cada elemento
+        for(char i: arrayMensaje){
+            valido = true;
+            Iterator itr = inicial.iterator();
+            //Si el elemento que le toca no fue contado ya, es valido contar su frecuencia
+            while (itr.hasNext()&&valido){
+                Nodo element = (Nodo) itr.next();
+                if (i==element.getCh()){
+                    valido = false;
+                }
+            }
+            //Contar la frecuencia del elemento
+            if (valido== true){
+                Nodo temp = new Nodo();
+                temp.setCh(i);
+                for (char j:arrayMensaje){
+                    if (j==i){
+                        contador++;
+                    }
+                }
+                temp.setFreq(contador);
+                inicial.add(temp);
+                contador = 0;
+              
+            }
         }
+        //Se recorre la lista generada de nodos para mostrar la frecuencia de cada elemento ene el orden en que aparecen
+        System.out.println("elemento | frecuencia");
+        Iterator itr = inicial.iterator();
+        while (itr.hasNext()){
+            Nodo element = (Nodo)itr.next();
+            System.out.println(element.getCh() + "          "+ element.getFreq());
+        }
+        
+        HuffmanTree huff = new HuffmanTree();
+        huff.createTree(inicial);
+        //System.out.println(huff.getRoot().getCh()+" "+huff.getRoot().getFreq());
     }
+   
+    
     
 }
